8077ae368681bb73beacb42e53af5f7846ec823f
annajliang <annaliangdev@gmail.com>
2020-08-01 02:55:02 -0400

    finish html for navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+import logo from "./assets/logo.png";
 
 const Navbar = () => {
-    return <nav>Navbar</nav>;
+    return (
+        <nav>
+            <div>
+                <a href="/">
+                    <img src={logo} alt="CommerceBear Logo" />
+                </a>
+            </div>
+            <ul>
+                <li>Solutions</li>
+                <li>Why Bear?</li>
+                <li>About</li>
+                <li>Pricing</li>
+                <li>FAQ</li>
+                <li>
+                    <a href="/">Get Started</a>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
