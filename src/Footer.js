71005fa3b55ab0efb79b68cd1275b5e27cd8ecdf
annajliang <annaliangdev@gmail.com>
2020-08-01 04:24:41 -0400

    add rest of text to footer

--- src/Footer.js
+++ src/Footer.js
@@ -13,9 +13,21 @@ const Footer = () => {
     <footer>
       <NavLinks />
       <div>
+        <p>hello@commercebear.com</p>
+        <p>Canada +1 647 479 6243</p>
+        <p>USA +1 929 232 2327</p>
+      </div>
+      <div>
         <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
         <FontAwesomeIcon icon={faInstagram} size="lg" />
         <FontAwesomeIcon icon={faTwitter} size="lg" />
+      </div>
+      <div>
+        <p>
+          <a href="/">Click here</a> to join our growing industry following and
+          stay up to date with all things CommerceBear.
+        </p>
+        <p>© 2020 CommerceBear. All Rights Reserved</p>
       </div>
     </footer>
   );
