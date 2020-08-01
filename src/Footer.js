38d0990d674de73ef512999cd25c2bde81475e99
annajliang <annaliangdev@gmail.com>
2020-08-01 04:23:32 -0400

    install fontawesome, import to footer component

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import NavLinks from './NavLinks';
+import React from "react";
+import NavLinks from "./NavLinks";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedinIn,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-    return (
-        <footer>
-            <NavLinks />
-        </footer>
-    );
+  return (
+    <footer>
+      <NavLinks />
+      <div>
+        <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
+        <FontAwesomeIcon icon={faInstagram} size="lg" />
+        <FontAwesomeIcon icon={faTwitter} size="lg" />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
