2a922c5b3e58b02cdb5501459cb55f975e788a44
annajliang <annaliangdev@gmail.com>
2020-08-01 05:19:11 -0400

    resize logo image

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import NavLinks from './NavLinks';
 import logo from "./assets/logo.png";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
     return (
         <nav>
             <div>
                 <a href="/">
-                    <img src={logo} alt="CommerceBear Logo" />
+                    <img src={logo} className={styles.logo} alt="CommerceBear Logo" />
                 </a>
             </div>
             <NavLinks />
