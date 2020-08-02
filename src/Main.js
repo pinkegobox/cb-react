3fe0090c668f0db27772950c5a56030d56f09264
annajliang <annaliangdev@gmail.com>
2020-08-02 01:13:15 -0400

    fix text alignment, add margins to main

--- src/Main.js
+++ src/Main.js
@@ -5,14 +5,16 @@ import styles from "./Main.module.css";
 const Main = () => {
     return (
         <main className={`wrapper ${styles.mainContainer}`}>
-            <h2>
-                Generate new revenue, profits, and establish an effective
-                omni-channelbusiness.
-            </h2>
-            <h2>Over 53,720 furniture SKUs published.</h2>
+            <div className={styles.desc}>
+                <h2 className={styles.h2}>
+                    Generate new revenue, profits, and establish an effective
+                    omni-channelbusiness.
+                </h2>
+                <h2>Over 53,720 furniture SKUs published.</h2>
+            </div>
             <img src={ecommerce} alt="" />
             <h2>Designed to drive sales and automate operations.</h2>
-            <ul>
+            <ul className={styles.featuresList}>
                 <li>
                     <h3>Multi Channel Publishing</h3>
                     <p>Fully built-out product data and content deployed to all channels</p>
