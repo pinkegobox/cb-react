afa02b5d718e072245c48774103012c6006df3d4
annajliang <annaliangdev@gmail.com>
2020-08-01 20:14:43 -0400

    apply background-color only on background-image

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import Navbar from './Navbar';
-import styles from './Header.module.css';
+import React from "react";
+import Navbar from "./Navbar";
+import styles from "./Header.module.css";
 
 const Header = () => {
-    return (
-        <header className={styles.banner}>
-            <div className={styles.colorOverlay}></div>
-            <Navbar />
-            <h1>Multichannel ecommerce designed for furniture manufacturers.</h1>
-        </header>
-    );
+  return (
+    <header className={styles.banner}>
+      <div className={styles.content}>
+        <Navbar />
+        <h1>Multichannel ecommerce designed for furniture manufacturers.</h1>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
