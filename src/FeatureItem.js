0517e00376972b8f01ee39730e0d0485e15d8303
annajliang <annaliangdev@gmail.com>
2020-08-02 15:09:36 -0400

    refactor features and make features item its own component

---
+++ src/FeatureItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./FeatureItem.module.css";
+
+const FeatureItem = (props) => {
+    return (
+      <li>
+        <img src={props.img} alt={`${props.title} icon`} />
+        <div className={styles.featuresDesc}>
+          <h3>{props.title}</h3>
+          <p>{props.desc}</p>
+        </div>
+      </li>
+    );
+};
+
+export default FeatureItem;
--- src/Features.js
+++ src/Features.js
@@ -4,53 +4,43 @@ import autoPromotions from "./assets/automated-promotions.png";
 import channelSetup from "./assets/channel-setup.png";
 import inventoryApp from "./assets/inventory-app.png";
 import supportPlus from "./assets/support-plus.png";
+import FeatureItem from "./FeatureItem";
 import styles from "./Features.module.css";
 
 const Features = () => {
     return (
       <ul className={styles.featuresList}>
-        <li>
-          <img src={multichannel} alt="" />
-          <div className={styles.featuresDesc}>
-            <h3>Multi Channel Publishing</h3>
-            <p>
-              Fully built-out product data and content deployed to all channels
-            </p>
-          </div>
-        </li>
-        <li>
-          <img src={inventoryApp} alt="" />
-          <div className={styles.featuresDesc}>
-            <h3>Inventory App</h3>
-            <p>Automated multi-marketplace inventory management</p>
-          </div>
-        </li>
-        <li>
-          <img src={autoPromotions} alt="" />
-          <div className={styles.featuresDesc}>
-            <h3>Automated Promotions</h3>
-            <p>
-              Full setup and optimization of unpaid promotions across all
-              channels
-            </p>
-          </div>
-        </li>
-        <li>
-          <img src={supportPlus} alt="" />
-          <div className={styles.featuresDesc}>
-            <h3>Support Plus</h3>
-            <p>
-              Troubleshooting, insight, and best practices to help you scale
-            </p>
-          </div>
-        </li>
-        <li>
-          <img src={channelSetup} alt="" />
-          <div className={styles.featuresDesc}>
-            <h3>Channel Setup</h3>
-            <p>Streamlined marketplace onboarding</p>
-          </div>
-        </li>
+        <FeatureItem
+          img={multichannel}
+          title={"Multi Channel Publishing"}
+          desc={
+            "Fully built-out product data and content deployed to all channels"
+          }
+        />
+        <FeatureItem
+          img={inventoryApp}
+          title={"Inventory App"}
+          desc={"Automated multi-marketplace inventory management"}
+        />
+        <FeatureItem
+          img={autoPromotions}
+          title={"Automated Promotions"}
+          desc={
+            "Full setup and optimization of unpaid promotions across all channels"
+          }
+        />
+        <FeatureItem
+          img={supportPlus}
+          title={"Support Plus"}
+          desc={
+            "Troubleshooting, insight, and best practices to help you scale"
+          }
+        />
+        <FeatureItem 
+          img={channelSetup} 
+          title={"Channel Setup"} 
+          desc={"Streamlined marketplace onboarding"} 
+        />
       </ul>
     );
 };
