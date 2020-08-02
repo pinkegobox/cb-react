32ffca68d7897ba0ae4cf3c8123713823c891170
annajliang <annaliangdev@gmail.com>
2020-08-02 04:07:54 -0400

    add inital styles to footer, center navlinks

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavLinks from "./NavLinks";
+import styles from "./Footer.module.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -10,24 +11,26 @@ import {
 
 const Footer = () => {
   return (
-    <footer>
-      <NavLinks />
-      <div>
-        <p>hello@commercebear.com</p>
-        <p>Canada +1 647 479 6243</p>
-        <p>USA +1 929 232 2327</p>
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
-        <FontAwesomeIcon icon={faInstagram} size="lg" />
-        <FontAwesomeIcon icon={faTwitter} size="lg" />
-      </div>
-      <div>
-        <p>
-          <a href="/">Click here</a> to join our growing industry following and
-          stay up to date with all things CommerceBear.
-        </p>
-        <p>© 2020 CommerceBear. All Rights Reserved</p>
+    <footer className={styles.footerContainer}>
+      <div className="wrapper">
+        <NavLinks />
+        <div className={styles.contact}>
+          <p>hello@commercebear.com</p>
+          <p>Canada +1 647 479 6243</p>
+          <p>USA +1 929 232 2327</p>
+        </div>
+        <div className={styles.socialLinks}>
+          <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
+          <FontAwesomeIcon icon={faInstagram} size="lg" />
+          <FontAwesomeIcon icon={faTwitter} size="lg" />
+        </div>
+        <div>
+          <p>
+            <a href="/">Click here</a> to join our growing industry following and
+            stay up to date with all things CommerceBear.
+          </p>
+          <p>© 2020 CommerceBear. All Rights Reserved</p>
+        </div>
       </div>
     </footer>
   );
