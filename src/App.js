5d1b0de4cdd421c69dc5f8ec2041dbe867d76148
annajliang <annaliangdev@gmail.com>
2020-08-03 02:53:53 -0400

    turn app into class component and header back into function component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Modal from "./Modal";
+import AnnouncementBar from "./AnnouncementBar";
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      {/* <Modal /> */}
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isBarVisible: true,
+      isModalVisible: true,
+    };
+  }
+
+  //checks if a value exists in isBarVisible and/or isModalVisible indicating whether the user has visited the page & interacted with the bar and modal before
+  //if the user has, then update the state of isBarVisible and/or isModalVisible to false to prevent announcement bar & modal from appearing again
+  componentDidMount() {
+    if (localStorage.getItem("isBarVisible")) {
+      this.setState({ isBarVisible: false });
+    }
+
+    if (localStorage.getItem("isModalVisible")) {
+      this.setState({ isModalVisible: false });
+    }
+  }
+
+  //once user clicks element, data about the user's visit is stored on their browser through localStorage
+  handleModalClick = () => {
+    this.setState({
+      isModalVisible: false,
+    });
+    localStorage.setItem("isModalVisible", false);
+  };
+
+  handleBarClick = () => {
+    this.setState({
+      isBarVisible: false,
+    });
+    localStorage.setItem("isBarVisible", false);
+  };
+
+  render() {
+   const { isModalVisible, isBarVisible } = this.state;
+
+   const renderModal = isModalVisible ? (
+     <Modal close={this.handleModalClick} />
+   ) : null;
+
+   const renderBar = isBarVisible ? (
+     <AnnouncementBar close={this.handleBarClick} />
+   ) : null;
+
+    return (
+      <div className="App">
+        {renderModal}
+        {renderBar}
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,72 +1,20 @@
-import React, { Component } from "react";
-import AnnouncementBar from "./AnnouncementBar";
-import Modal from "./Modal";
+import React from "react";
 import Navbar from "./Navbar";
 import styles from "./Header.module.css";
 
-class Header extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isBarVisible: true,
-      isModalVisible: true,
-    };
-  }
-
-  //checks if a value exists in isBarVisible and/or isModalVisible indicating whether the user has visited the page & interacted with the bar and modal before
-  //if the user has, then update the state of isBarVisible and/or isModalVisible to false to prevent announcement bar & modal from appearing again
-  componentDidMount() {
-    if (localStorage.getItem("isBarVisible")) {
-      this.setState({ isBarVisible: false });
-    }
-
-    if (localStorage.getItem("isModalVisible")) {
-      this.setState({ isModalVisible: false });
-    }
-  }
-
-  //once user clicks element, data about the user's visit is stored on their browser through localStorage
-  handleModalClick = () => {
-    this.setState({
-      isModalVisible: false,
-    });
-    localStorage.setItem("isModalVisible", false);
-  };
-
-  handleBarClick = () => {
-    this.setState({
-      isBarVisible: false,
-    });
-    localStorage.setItem("isBarVisible", false);
-  };
-
-  render() {
-    const { isModalVisible, isBarVisible } = this.state;
-
-    const renderModal = isModalVisible ? (
-      <Modal close={this.handleModalClick} />
-    ) : null;
-
-    const renderBar = isBarVisible ? (
-      <AnnouncementBar close={this.handleBarClick} />
-    ) : null;
-
-    return (
-      <header className={styles.banner}>
-        <div className={styles.content}>
-          {renderModal}
-          {renderBar}
-          <Navbar />
-          <div className={styles.headingContainer}>
-            <h1 className={`wrapper ${styles.h1}`}>
-              Multichannel ecommerce designed for furniture manufacturers.
-            </h1>
-          </div>
-        </div>
-      </header>
-    );
-  }
+const Header = () => {
+   return (
+     <header className={styles.banner}>
+       <div className={styles.content}>
+         <Navbar />
+         <div className={styles.headingContainer}>
+           <h1 className={`wrapper ${styles.h1}`}>
+             Multichannel ecommerce designed for furniture manufacturers.
+           </h1>
+         </div>
+       </div>
+     </header>
+   );
 }
-
 
 export default Header;
