803f8c13cffe105a406ed8f9cbb79c44836760da
annajliang <annaliangdev@gmail.com>
2020-08-03 02:08:15 -0400

    finish styling modal component

--- src/Modal.js
+++ src/Modal.js
@@ -12,16 +12,16 @@ const Modal = (props) => {
         aria-label="Close Announcement"
       ></span> */}
       <CloseButton close={props.close} />
-      <h2>Don't miss a thing.</h2>
-      <p>
+      <h2 className={styles.h2}>Don't miss a thing.</h2>
+      <p className={styles.desc}>
         Sign up for our mailing list and get industry updates, live events, and
         great analysis.
       </p>
-      <form action="#">
+      <form action="#" className={styles.form}>
         <input type="text" placeholder="Email Address" />
         <input type="button" value="Sign Up" />
       </form>
-      <p>We respect your privacy.</p>
+      <p className={styles.formDisclaimer}>We respect your privacy.</p>
     </div>
   );
 };
